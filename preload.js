41f8122f80fd8d77a44b84163d4b584238387a3a
onebitetechnology <80067352+onebitetechnology@users.noreply.github.com>
2026-03-16 15:35:55 -0600

    Release 2.1.3

--- preload.js
+++ preload.js
@@ -2,8 +2,11 @@ const { contextBridge, ipcRenderer } = require('electron');
 
 contextBridge.exposeInMainWorld('desktopApp', {
   getMetadata: () => ipcRenderer.invoke('app:get-metadata'),
+  listDisplays: () => ipcRenderer.invoke('app:list-displays'),
   applyWindowPreferences: (preferences) => ipcRenderer.invoke('window:apply-preferences', preferences),
   openInBrowser: () => ipcRenderer.invoke('app:open-in-browser'),
+  openFeatureRequest: () => ipcRenderer.invoke('app:open-feature-request'),
   getUpdateStatus: () => ipcRenderer.invoke('updates:get-status'),
   checkForUpdates: () => ipcRenderer.invoke('updates:check'),
+  installUpdate: () => ipcRenderer.invoke('updates:install'),
 });
